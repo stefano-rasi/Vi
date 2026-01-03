3696e2205fb4168c18fac964bfd7f6407ef00c9e
Stefano Rasi <rasi.stefano@gmail.com>
2026-01-03 07:09:01 +0100

    Fix View require path

--- vi.rb
+++ vi.rb
@@ -1,6 +1,6 @@
-require 'lib/View/html'
-require 'lib/View/view'
-require 'lib/View/window'
+require 'lib/view/html'
+require 'lib/view/view'
+require 'lib/view/window'
 
 require_relative 'mode/insert'
 require_relative 'mode/normal'
